da0fc231bb920abb966ce9838de5961442eb2e89
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2012-07-18 17:48:25 +0200

    Update snowball-ics-gcc46-igloo-stable-blob.mk
    Snapshot from android-build.linaro.org

--- snowball-ics-gcc46-igloo-stable-blob.mk
+++ snowball-ics-gcc46-igloo-stable-blob.mk
@@ -5,16 +5,16 @@ MANIFEST_FILENAME=landing-snowball.xml
 TARGET_PRODUCT=snowball
 TARGET_SIMULATOR=false
 TARGET_BUILD_VARIANT=tests
-TOOLCHAIN_URL=http://snapshots.linaro.org/android/~linaro-android/toolchain-4.6-2012.05/1/android-toolchain-eabi-linaro-4.6-2012.05-1-2012-05-18_15-55-36-linux-x86.tar.bz2
+TOOLCHAIN_URL=http://snapshots.linaro.org/android/~linaro-android/toolchain-4.6-2012.07/1/android-toolchain-eabi-linaro-4.6-2012.07-1-2012-07-16_08-54-46-linux-x86.tar.bz2
 TOOLCHAIN_TRIPLET=arm-linux-androideabi
 REPO_SEED_URL=http://android-build.linaro.org/seed/uniseed.tar.gz
-SOURCE_OVERLAY=snowball/20120531/vendor.tar.bz2
+SOURCE_OVERLAY="snowball/20120531/vendor.tar.bz2"
 LAVA_SUBMIT=1
 LAVA_SUBMIT_FATAL=0
-LAVA_TEST_PLAN="busybox,0xbench,glmark2,skia,v8,mmtest,cts,monkey,monkey_long_run"
+LAVA_TEST_PLAN="busybox,0xbench,glmark2,skia,v8,mmtest,monkey,monkey_long_run"
 SYNC_JOBS=10
 MONKEY_RUNNER_URL_1="git://android.git.linaro.org/test/linaro/android/system.git"
-MONKEY_RUNNER_URL_2="git://android.git.linaro.org/linaro/benchmarks.git"
+MONKEY_RUNNER_URL_2="file:///home/vishalbhoj/benchmarks"
 CTS_TIMEOUT=36000
 
 # List of non-standard variables to pass to android make
