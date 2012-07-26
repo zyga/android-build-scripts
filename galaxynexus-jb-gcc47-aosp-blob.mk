0a6568c638969eda387193cec9ea4e1a9e7127f9
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2012-07-26 22:58:50 +0200

    Add galaxy nexus jelly bean config

---
+++ galaxynexus-jb-gcc47-aosp-blob.mk
@@ -0,0 +1,10 @@
+# Vanilla config from linaro android build system
+MANIFEST_REPO=git://android.git.linaro.org/platform/manifest.git
+MANIFEST_BRANCH=linaro_android_4.1.1
+MANIFEST_FILENAME=default.xml
+TARGET_PRODUCT=full_maguro
+TARGET_BUILD_VARIANT=eng
+MAKE_TARGETS=droid
+TOOLCHAIN_URL=http://snapshots.linaro.org/android/~linaro-android/toolchain-4.7-2012.07/1/android-toolchain-eabi-linaro-4.7-2012.07-1-2012-07-16_08-48-35-linux-x86.tar.bz2
+TOOLCHAIN_TRIPLET=arm-linux-androideabi
+REPO_SEED_URL=http://android-build.linaro.org/seed/uniseed.tar.gz
